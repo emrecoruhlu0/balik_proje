5d28bc3f595eecf6859e45075558039f47d3b4f5
samethabali <samets.250223@gmail.com>
2025-11-21 16:36:22 +0300

    Proje baslangici: Backend ve Frontend yapisi kuruldu

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+// frontend/src/App.jsx
+import React from 'react';
+import MapComponent from './components/MapComponent';
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0 }}>
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
